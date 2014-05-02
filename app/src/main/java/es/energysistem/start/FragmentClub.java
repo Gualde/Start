23c2a9dcaa976bc9629408d37756c5d29872df68
gualde <gualde@gmail.com>
2014-05-02 18:48:03 +0200

    ubication service

--- app/src/main/java/es/energysistem/start/FragmentClub.java
+++ app/src/main/java/es/energysistem/start/FragmentClub.java
@@ -1,5 +1,6 @@
 package es.energysistem.start;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ public class FragmentClub extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+
         return inflater.inflate(R.layout.fragment_club,container, false);
 
     }
